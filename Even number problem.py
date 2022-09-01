633357fa4df8bd4010e723f5d3dd0e5a8df809d6
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-09-01 23:39:24 +0900

    Even number practice problem
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/9/1
    # Problem: Even number practice problem

---
+++ Even number problem.py
@@ -0,0 +1,9 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/9/1
+# Problem: Even number practice problem 
+
+a=int(input())
+b=int(input())
+for i in range (1,a+1)
+    if a%2==0 and b%2==0:
+        print(i)
